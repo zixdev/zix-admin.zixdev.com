d9013f270bbdb971c337e89cdbe627184e765a4e
badi ifaoui <badi1wow@gmail.com>
2016-12-31 16:13:57 +0100

    testing vue

--- src/modules/core/views/home/index.vue
+++ src/modules/core/views/home/index.vue
@@ -1,57 +1,34 @@
-<template>
-    <div>
-        <div class="row">
+<template lang="jade">
+    .row
+        .col-md-12
+            alert(type='success', dismissable='')
+                | hoho
 
-            <div class="col-lg-4">
-                <div class="ibox float-e-margins">
-                    <div class="ibox-title">
-                        <span class="label label-primary pull-right">Today</span>
-                        <h5>visits</h5>
-                    </div>
-                    <div class="ibox-content">
-                        <h1 class="no-margins">22 285,400</h1>
-                        <div class="stat-percent font-bold text-navy">20% <i class="fa fa-level-up"></i></div>
-                        <small>New orders</small>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="ibox float-e-margins">
-                    <div class="ibox-title">
-                        <span class="label label-info pull-right">Monthly</span>
-                        <h5>Orders</h5>
-                    </div>
-                    <div class="ibox-content">
-                        <h1 class="no-margins">60 420,600</h1>
-                        <div class="stat-percent font-bold text-info">40% <i class="fa fa-level-up"></i></div>
-                        <small>New orders</small>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="ibox float-e-margins">
-                    <div class="ibox-title">
-                        <span class="label label-warning pull-right">Annual</span>
-                        <h5>Income</h5>
-                    </div>
-                    <div class="ibox-content">
-                        <h1 class="no-margins">$ 120 430,800</h1>
-                        <div class="stat-percent font-bold text-warning">16% <i class="fa fa-level-up"></i></div>
-                        <small>New orders</small>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+            alert(type='danger') {{ home }}
+                
+
+
 </template>
-<script>
+<script type="text/babel">
     import Component from 'vue-class-component'
     import Vue from "vue";
+    import { alert } from 'vue-strap';
 
-    @Component
+    @Component({
+        components: {
+            alert
+        }
+    })
     export default class Home {
         created() {
+            console.log()
+            console.info('hoho')
 
+
+        }
+
+        get home() {
+            return 'hello world';
         }
 
         change() {
