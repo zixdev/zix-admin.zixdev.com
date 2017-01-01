cdaa7e1f13232bdf55b1e2af1525a0f2a85ac898
badi ifaoui <badi1wow@gmail.com>
2017-01-01 10:18:13 +0100

    updated: header

--- src/modules/core/components/layouts/AppHeader.vue
+++ src/modules/core/components/layouts/AppHeader.vue
@@ -1,34 +1,38 @@
 <template lang="jade">
-.row.border-bottom
-  nav.navbar.navbar-static-top.white-bg(role='navigation', style='margin-bottom: 0')
-    .navbar-header
-      a.navbar-minimalize.minimalize-styl-2.btn.btn-primary(href='javascript:void(0);')
-        i.fa.fa-bars
-      form.navbar-form-custom(role='search', method='post', action='#')
-        .form-group
-          input#top-search.form-control(type='text', :placeholder="$t('search.main')", name='top-search')
-    ul.nav.navbar-top-links.navbar-right
-      li.dropdown
-        a.dropdown-toggle.count-info(data-toggle='dropdown', href='#', aria-expanded='false')
-          | {{ $t('lang') }}
-        ul.dropdown-menu.dropdown-messages
-          li
-            a(@click="changeLang('en')") English
-          li
-            a(@click="changeLang('fr')") French
-          li
-            a(@click="changeLang('ar')") Arabic
-      li
-        router-link(:to="{name: 'auth.logout'}")
-          i.fa.fa-sign-out
-          |  {{$t('auth.logout')}}
+  .row.border-bottom
+    nav.navbar.navbar-static-top.white-bg(role='navigation', style='margin-bottom: 0')
+      .navbar-header
+        a.navbar-minimalize.minimalize-styl-2.btn.btn-primary(href='javascript:void(0);')
+          i.fa.fa-bars
+        form.navbar-form-custom(role='search', method='post', action='#')
+          .form-group
+            input#top-search.form-control(type='text', :placeholder="$t('search.main')", name='top-search')
+      ul.nav.navbar-top-links.navbar-right
+        li.dropdown
+          a.dropdown-toggle.count-info(data-toggle='dropdown', href='#', aria-expanded='false')
+            | {{ $t('lang') }}
+          ul.dropdown-menu.dropdown-messages
+            li
+              a(@click="changeLang('en')") English
+            li
+              a(@click="changeLang('fr')") French
+            li
+              a(@click="changeLang('ar')") Arabic
+
+        li
+          router-link(:to="{name: 'auth.logout'}")
+            i.fa.fa-sign-out
+            |  {{$t('auth.logout')}}
+
+
 
 
 </template>
 
 <script type="text/bable">
-  import Vue from 'vue';
-  import Component from "vue-class-component";
+    import Vue from 'vue';
+    import Component from "vue-class-component";
+
     @Component
     export default class AppHeader {
         changeLang(lang) {
