e1654fb56f546a76de1abffa4c0f44433321882c
badi ifaoui <badi1wow@gmail.com>
2017-01-02 11:37:29 +0100

    updated: tabs component

--- src/styles/zix-components/index.js
+++ src/styles/zix-components/index.js
@@ -3,13 +3,11 @@ import Vue from 'vue';
 import Alert from './Alert'
 import Dropdown from './Dropdown'
 import Collapse from './Collapse'
-// import Tab from './Tab'
-// import TabSet from './Tabset'
-// import TabGroup from './TabGroup'
+import Tab from './Tab'
+import TabSet from './Tabset'
 
 Vue.component('alert', Alert);
 Vue.component('dropdown', Dropdown)
 Vue.component('collapse', Collapse)
-// Vue.component('tabs', TabSet)
-// Vue.component('tab', Tab)
-// Vue.component('tab-group', TabGroup)
+Vue.component('tabs', TabSet)
+Vue.component('tab', Tab)
